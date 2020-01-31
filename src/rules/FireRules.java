b23eab5da3ca2f8358e5c5e81fa41153558050b4
Grant LoPresti <gjl13@duke.edu>
2020-01-31 12:51:41 -0500

    get color changes to fire rules

--- src/rules/FireRules.java
+++ src/rules/FireRules.java
@@ -3,14 +3,40 @@ package rules;
 import javafx.scene.paint.Color;
 
 public class FireRules extends Rules {
+  private double fireSpreadProbability;
 
+  public FireRules(double spreadProb) {
+    super();
+    fireSpreadProbability = spreadProb;
+
+  }
+  /**
+   * 0 corresponds to Burnt, 1 Corresponds to Alive, 2 Corresponds to Burning
+   * @param currentState
+   * @param neighbors
+   * @return
+   */
   @Override
   public int calculateNewState(int currentState, int[] neighbors) {
+    //TODO
+    // - add code to calculate new state of cell
+
     return 0;
   }
 
+  /**
+   * Burnt is Black, Alive is Green, Burning is Red
+   * @param state
+   * @return
+   */
   @Override
   public Color getStateColor(int state) {
-    return null;
+    if (state == 0) {
+      return Color.BLACK;
+    } else if (state == 1) {
+      return Color.GREEN;
+    } else {
+      return Color.RED;
+    }
   }
 }
