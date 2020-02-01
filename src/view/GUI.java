37779b4c98f2b9cf46025d061a308f1bec1f17c1
Sebastian Williams <svw9@duke.edu>
2020-02-01 17:16:11 -0500

    Made rectangle grid; started working on AnimationTimer

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -5,7 +5,10 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
@@ -24,12 +27,19 @@ public class GUI extends Application {
 
     // Instance variables for the GUI
     private Stage mainWindow;
-    private Group root;
-    private Scene scene;
-    private Canvas canvas;
+    private GridPane gp;
     private Simulation simulation;
     private String simulationTitle;
     //private Simulation simulation;
+
+    // Buttons members
+    private Button myHomeButton;
+    private Button myResetButton;
+    private Button mySlowDownButton;
+    private Button myPauseButton;
+    private Button mySpeedUpButton;
+    private Button myStepButton;
+
     public static void main(String[] args) {
         Application.launch(args);
     }
@@ -38,61 +48,80 @@ public class GUI extends Application {
     public void start(Stage primaryStage) throws Exception {
         setUpWindow(primaryStage);
         loadSimulation();
-        GridAnimation ga = new GridAnimation();
+        mainWindow.show();
+        new AnimationTimer(){
+
+            @Override
+            public void handle(long now) {
+                step();
+            }
+        }.start();
     }
-    private void makeButton(){
-        Button btn = new Button();
-        btn.setText("Test");
-        root.getChildren().add(btn);
+    private void step(){
+        makeMasterGrid();
     }
     private void setUpWindow(Stage primaryStage){
         mainWindow = primaryStage;
         mainWindow.setTitle(WINDOW_TITLE);
-        root = new Group();
-        scene = new Scene(root);
-        mainWindow.setScene(scene);
-        canvas = new Canvas(512, 512);
-        root.getChildren().add(canvas);
+
+        Scene gridScene = new Scene(makeMasterGrid(), 512, 512);
+        mainWindow.setScene(gridScene);
+
     }
+    private GridPane makeMasterGrid(){
+        gp = new GridPane();
+        makeButtons();
+        makeGrid();
+        return gp;
+    }
+    private GridPane makeButtons(){
+        myHomeButton = new Button("Home");
+        gp.add(myHomeButton, 0,0);
+        // command for going home
+
+        myResetButton = new Button("Reset");
+        gp.add(myResetButton, 1, 0);
+        // command for reset
+
+        mySlowDownButton = new Button("Slow Down");
+        mySlowDownButton.setOnAction(e -> simulation.slowDown());
+        gp.add(mySlowDownButton, 2, 0);
+
+        myPauseButton = new Button("Pause");
+        myPauseButton.setOnAction(e -> simulation.pause());
+        gp.add(myPauseButton, 3, 0);
+
+        mySpeedUpButton = new Button("Speed Up");
+        mySpeedUpButton.setOnAction(e -> simulation.speedUp());
+        gp.add(mySpeedUpButton, 4, 0);
+
+        myStepButton = new Button("Step");
+        myStepButton.setOnAction(e -> simulation.step());
+        gp.add(myStepButton, 5, 0);
+
+        return gp;
+    }
+    private GridPane makeGrid(){
+        // Color[][] colorGrid = simulation.getColorGrid();
+        Color[][] colorGrid = {{Color.BLACK, Color.GREEN, Color.RED},{Color.RED, Color.GREEN, Color.RED}, {Color.RED, Color.RED, Color.RED}};
+        int width = colorGrid.length;
+        int height = colorGrid[0].length;
+        for(int i = 0; i < width; i++){
+            for(int j = 0; j < height; j++){
+                Rectangle rec = new Rectangle();
+                rec.setFill(colorGrid[i][j]);
+                rec.setWidth(50);
+                rec.setHeight(50);
+                gp.add(rec, i, j+1);
+            }
+        }
+        return gp;
+    }
+
     private void loadSimulation() throws ParserConfigurationException, SAXException, IOException {
         simulation = new Simulation("data/generatedXML.xml");
         simulationTitle = simulation.getTitle();
         //you can use simulation.getColorGrid() to get a Color[][] for each cell's state
     }
-    private class GridAnimation extends AnimationTimer{
 
-        @Override
-        public void handle(long now) {
-            TilePane tp = new TilePane();
-            Rectangle rec = new Rectangle();
-            Rectangle rec2 = new Rectangle();
-            Rectangle rec3 = new Rectangle();
-            Rectangle rec4 = new Rectangle();
-            tp.getChildren().add(rec);
-            tp.getChildren().add(rec2);
-            tp.getChildren().add(rec3);
-            tp.getChildren().add(rec4);
-
-            //get grid
-            //draw grid
-        }
-    }
-    private Color[][] parseGridColor(char[][] grid){
-        int gWidth = grid[0].length;
-        int gLength = grid.length;
-        Color[][] colorGrid = new Color[gLength][gWidth];
-        for(int i = 0; i < gWidth; i++){
-            for(int j = 0; j < gWidth; j++){
-                switch (grid[i][j]){
-                    case 'b':
-                        colorGrid[i][j] = Color.BLUE;
-                    case 'r':
-                        colorGrid[i][j] = Color.RED;
-                    case 'g':
-                        colorGrid[i][j] = Color.GREEN;
-                }
-            }
-        }
-        return colorGrid;
-    }
 }
