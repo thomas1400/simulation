24005e1f2a1a1ec02bef375b46e4cc907ffb3c0b
Thomas Owens <to57@duke.edu>
2020-02-02 15:31:49 -0500

    Updated SegregationRules

--- src/rules/SegregationRules.java
+++ src/rules/SegregationRules.java
@@ -4,9 +4,28 @@ import javafx.scene.paint.Color;
 
 public class SegregationRules extends Rules {
 
+  private double segregation_threshold;
+
   @Override
   public int calculateNewState(int currentState, int[] neighbors) {
-    return 0;
+    if (percentUnlikeNeighbors(currentState, neighbors) > segregation_threshold) {
+      return -1*(int)(Math.random() * 8);
+    } else {
+      return currentState;
+    }
+  }
+
+  private double percentUnlikeNeighbors(int currentState, int[] neighbors) {
+    if (currentState == 0) {
+      return 0;
+    }
+    int likeNeighbors = 0;
+    for (int n : neighbors) {
+      if ( n == currentState) {
+        likeNeighbors += 1;
+      }
+    }
+    return likeNeighbors / 8.0;
   }
 
   @Override
@@ -16,6 +35,6 @@ public class SegregationRules extends Rules {
 
   @Override
   public void setGlobalVariables(double[] variables) {
-
+    segregation_threshold = variables[0];
   }
 }
