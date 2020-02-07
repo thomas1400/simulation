d307fd2d14f209a93b138cbe37cc1e12b0879ef6
Grant LoPresti <gjl13@duke.edu>
2020-02-07 13:32:02 -0500

    created new grid class and immutable grid interface

--- src/simulation/xmlGeneration/UserInputParser.java
+++ src/simulation/xmlGeneration/UserInputParser.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class UserInputParser {
   
-  public static String xmlFilePath;
+  private static String xmlFilePath;
   private static String myRuleSelector;
   private static String mySimulationTitle;
   private static String mySimulationAuthor;
