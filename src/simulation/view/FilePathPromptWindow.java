1d60b2474f65bcdc70eddef85f94857702097c77
Grant LoPresti <gjl13@duke.edu>
2020-02-10 01:30:54 -0500

    custom settings window created by inoperable

--- src/simulation/view/FilePathPromptWindow.java
+++ src/simulation/view/FilePathPromptWindow.java
@@ -7,7 +7,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import simulation.controller.Simulation;
 
@@ -40,7 +42,12 @@ public class FilePathPromptWindow extends Application {
     master.setHgap(5);
     master.setVgap(10);
 
-    master.add(new Label("File Path Name:"), 0, 0);
+    HBox hb = new HBox();
+    Label label = new Label("File Path");
+    TextField textField = new TextField();
+    hb.getChildren().addAll(label, textField);
+    hb.setSpacing(10);
+    master.add(hb, 0,1);
 
     master.setPadding(new Insets(PADDING));
 
